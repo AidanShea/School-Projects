561ae7b5aef7f0e6b75205c9ca191f2c1b73dfb1
AidanShea <siraidanshea@gmail.com>
2017-03-03 11:21:44 -0500

    Add files via upload

--- Palindromee/src/Palindrome_Detection.java
+++ Palindromee/src/Palindrome_Detection.java
@@ -5,37 +5,48 @@ public class Palindrome_Detection {
 		boolean asking = true;
 		Scanner scanner = new Scanner(System.in);
 		String palindrome;
+		Stack palinStack = new Stack();
 		
 		while(asking){
 				System.out.println("Please enter a word");
 				palindrome = scanner.nextLine();
-		
+				
 				if(palindrome.contains("quit")){
 					asking = false;
 				} else if(checkPalindrome(palindrome)){
 					System.out.println("This is a palindrome!");
+					palinStack.push(palindrome);
 				} else {
 					System.out.println("This is not a palindrome.");
 				}
 		}
 		
-		System.out.println("Thanks for playing");
+		System.out.println("Thanks for playing, here are your successful palindromes!");
+		while(palinStack.size()>0){
+			System.out.println(palinStack.pop());
+		}
 	}
 	
 	public static boolean checkPalindrome(String palindrome) {
-		palindrome.toLowerCase();
+		palindrome = palindrome.toLowerCase();
 		Stack s = new Stack();
 		Queue q = new Queue();
 		String emordnilap = "";
 		int i = 0;
 		
 		while(i < palindrome.length()){
-			s.push(palindrome.substring(i, i+1));
+			if(Character.isLetter(palindrome.charAt(i))){
+				s.push(palindrome.substring(i, i+1));
+			}
 			i++;
 		}
 		
+		String letter;
+		palindrome = "";
 		while(s.size()!= 0){
-			q.enqueue(s.pop());
+			letter = (String) s.pop();
+			palindrome = letter + palindrome;
+			q.enqueue(letter);
 		}
 		
 		while(q.size()!=0){
