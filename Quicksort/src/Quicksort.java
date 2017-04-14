e9f293f6547a35dde19c1d1ee54675a891d31691
AidanShea <siraidanshea@gmail.com>
2017-04-13 23:22:56 -0400

    Add files via upload

--- Quicksort/src/Quicksort.java
+++ Quicksort/src/Quicksort.java
@@ -1,46 +1,77 @@
 
 public class Quicksort<E extends Comparable<E>> {
-	
-	public static void quicksort(Comparable[] array){
-		Comparable first = array[0];
-		Comparable last = array[array.length-1];
-		Comparable middle = array[array.length/2];
-		Comparable temp;
-		
-		if(first.compareTo(middle) == 1){
-			temp = middle;
-			middle = first;
-			array[array.length/2] = middle;
-			first = temp;
-			array[0] = first;
-		}
-		
-		if(middle.compareTo(last) == 1){
-			temp = last;
-			last = middle;
-			array[array.length-1] = last;
-			middle = temp;
-			array[array.length/2] = middle;
-			
-			if(first.compareTo(middle) == 1){
-				temp = middle;
-				middle = first;
-				array[array.length/2] = middle;
-				first = temp;
-				array[0] = first;
-			}
-		}
-		partition(array, middle);
+	public static void sort(Comparable[] array){
+		sort(array, 0, array.length-1);
 	}
 	
-	public static void partition(Comparable[] array, Comparable pivot){
-		Comparable temp = array[0];
-		array[0]= pivot;
-		array[array.length/2] = temp;
+	public static void sort(Comparable[] array, int first, int last){
+		if((last - first) > 64){
+			quicksort(array, first, last);
+		} else {
+			insertionSort(array, first, last);
+		}
+	}
+	
+	public static void insertionSort(Comparable[] array, int first, int last){
+		int i = first+1;
+		int j;
+		Comparable temp;
+		
+		while(i<=last){
+			j = i;
+			temp = array[i];
+			while(j >= 0){
+				if(temp.compareTo(array[j]) < 0){
+					array[j+1] = array[j];
+					array[j] = temp;
+				}
+				j--;
+			}
+			i++;
+		}
+	}
+	
+	public static void quicksort(Comparable[] array, int first, int last){
+		Comparable firstItem = array[first];
+		Comparable lastItem = array[last];
+		Comparable middleItem = array[(last-first)/2];
+		Comparable temp;
+		
+		if(firstItem.compareTo(middleItem) == 1){
+			temp = middleItem;
+			middleItem = firstItem;
+			array[(last-first)/2] = middleItem;
+			firstItem = temp;
+			array[first] = firstItem;
+		}
+		
+		if(middleItem.compareTo(lastItem) == 1){
+			temp = lastItem;
+			lastItem = middleItem;
+			array[last] = lastItem;
+			middleItem = temp;
+			array[(last-first)/2] = middleItem;
+			
+			if(firstItem.compareTo(middleItem) == 1){
+				temp = middleItem;
+				middleItem = firstItem;
+				array[(last-first)/2] = middleItem;
+				firstItem = temp;
+				array[first] = firstItem;
+			}
+		}
+		
+		partition(array, middleItem, first, last);
+	}
+	
+	public static void partition(Comparable[] array, Comparable pivot, int first, int last){
+		Comparable temp = array[first];
+		array[first]= pivot;
+		array[(last-first)/2] = temp;
 		
 		boolean resolved = false;
-		int i = 1;
-		int j = array.length-1;
+		int i = first+1;
+		int j = last;
 		
 		while(i<j){
 			if(array[i].compareTo(pivot) == 1 && array[j].compareTo(pivot) == -1){
@@ -61,30 +92,28 @@ public class Quicksort<E extends Comparable<E>> {
 		
 		temp = array[j];
 		array[j] = pivot;
-		array[0] = temp;
+		array[first] = temp;
+
+		
+		sort(array, first, j);
+		sort(array, j, last);
 	}
 	
 	public static void main(String args[]){
-		Comparable[] list = new Comparable[10];
+		Comparable[] array = new Comparable[128];
 		
-		list[0] = 7;
-		list[1] = 2;
-		list[2] = 3;
-		list[3] = 8;
-		list[4] = 4;
-		list[5] = 1;
-		list[6] = 5;
-		list[7] = 9;
-		list[8] = 6;
-		list[9] = 10;
-		
-		for(Comparable i: list){
-			System.out.print(i + ", ");	
+		for(int n = 0; n < 128; n++){
+			array[n] = (int) (Math.random()*200);
 		}
 		
-		quicksort(list);
+		for(Comparable i: array){
+			System.out.print(i + ", ");	
+		}
 		System.out.println("");
-		for(Comparable i: list){
+		
+		sort(array);
+	
+		for(Comparable i: array){
 			System.out.print(i + ", ");	
 		}
 	}
